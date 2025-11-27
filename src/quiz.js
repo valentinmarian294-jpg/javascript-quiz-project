311a1b7734a6ea61227793b654d97e505f3922b6
Valentin <valentinmarian294@gmail.com>
2025-11-27 17:58:18 +0100

    Quiz project

--- src/quiz.js
+++ src/quiz.js
@@ -1,6 +1,4 @@
 class Quiz {
-  // YOUR CODE HERE:
-  //
   constructor(questions, timeLimit, timeRemaining) {
     this.questions = questions;
     this.timeLimit = timeLimit;
@@ -8,15 +6,15 @@ class Quiz {
     this.correctAnswers = 0;
     this.currentQuestionIndex = 0;
   }
-
+  // 2. getQuestion()
   getQuestion() {
     return this.questions[this.currentQuestionIndex];
   }
-
+  // 3. moveToNextQuestion()
   moveToNextQuestion() {
     return this.currentQuestionIndex++;
   }
-
+  // 4. shuffleQuestions()
   shuffleQuestions() {
     for (let i = this.questions.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
@@ -25,11 +23,15 @@ class Quiz {
       this.questions[j] = temp;
     }
   }
-
+  // 5. checkAnswer(answer)
   checkAnswer(answer) {
-    return this.correctAnswers++;
+    if (answer === this.currentQuestionIndex.answer) {
+      return this.correctAnswers++;
+    } else {
+      return this.correctAnswers
+    }
   }
-
+  // 6. hasEnded()
   hasEnded() {
     if (this.currentQuestionIndex < this.questions.length) {
       return false;
@@ -37,14 +39,16 @@ class Quiz {
       return true;
     }
   }
-  filterQuestionsByDifficulty(difficulty){
-     if (typeof difficulty !== 'number' || difficulty < 1 || difficulty > 3){
+  filterQuestionsByDifficulty(difficulty) {
+    if (typeof difficulty !== "number" || difficulty < 1 || difficulty > 3) {
       return;
     }
-     this.questions = this.questions.filter(question => question.difficulty === difficulty);
+    this.questions = this.questions.filter(
+      (question) => question.difficulty === difficulty
+    );
   }
-  averageDifficulty(){
-  const selectedQuestions = this.questions.filter(
+  averageDifficulty() {
+    const selectedQuestions = this.questions.filter(
       (question) => typeof question.difficulty === "number"
     );
     const totalDifficulty = selectedQuestions.reduce(
@@ -57,5 +61,4 @@ class Quiz {
       return 0;
     }
   }
-  }
-   
+}
